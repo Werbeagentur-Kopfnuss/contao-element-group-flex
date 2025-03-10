dbbaf3b39a033ae6ea01181e06ff90f725910e21
Fritz Michael Gschwantner <fmg@inspiredminds.at>
2025-03-10 13:43:13 +0000

    Various improvements and simplifications (#1)

---
+++ contao/languages/en/tl_content.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+$GLOBALS['TL_LANG']['tl_content']['custom_flex_legend'] = 'Welche Flex-Option soll verwendet werden?';
+$GLOBALS['TL_LANG']['tl_content']['flex'] = ['Flex-Option', 'Bitte wählen Sie eine Flex-Option aus.'];
+$GLOBALS['TL_LANG']['tl_content']['flexOptions'] = [
+    'flex-50' => '2 Elemente nebeneinander',
+    'flex-30' => '3 Elemente nebeneinander',
+    'flex-25' => '4 Elemente nebeneinander',
+    'flex-16' => '6 Elemente nebeneinander',
+];
