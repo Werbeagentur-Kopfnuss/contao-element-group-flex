dcf1f545c32bf01db9c5311d5a8aa6bec808633b
fritzmg <fmg@inspiredminds.at>
2025-03-10 14:01:39 +0000

    wip

---
+++ src/EventListener/AddBackendCssListener.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace agenturkopfnuss\ContaoElementGroupFlex\EventListener;
+
+use Contao\CoreBundle\Routing\ScopeMatcher;
+use Symfony\Component\Asset\Packages;
+use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+
+#[AsEventListener]
+class AddBackendCssListener
+{
+    public function __construct(
+        private readonly ScopeMatcher $scopeMatcher,
+        private readonly Packages $packages,
+    )
+    {
+    }
+
+    private function __invoke(RequestEvent $event): void
+    {
+        if (!$this->scopeMatcher->isBackendMainRequest($event)) {
+            return;
+        }
+
+        $GLOBALS['TL_CSS'][] = $this->packages->getUrl('backend.css', 'agenturkopfnuss/contao-element-group-flex');
+    }
+}
