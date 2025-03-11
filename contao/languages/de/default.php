15643d8a01bf49de4a8ec6232055c19e305dbed7
Werbeagentur-Kopfnuss <marco@werbeagentur-kopfnuss.de>
2025-03-11 11:23:48 +0100

    [CHG] language

---
+++ contao/languages/de/default.php
@@ -0,0 +1,5 @@
+<?php
+
+declare(strict_types=1);
+
+$GLOBALS['TL_LANG']['CTE']['element_group_flex'] = ['Elementgruppe Flex', 'Ein Element um mehrere Elemente in einer flexiblen Gruppe nebeneinenader anzuzeigen.'];
--- contao/languages/en/default.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-$GLOBALS['TL_LANG']['CTE']['element_group_flex'] = ['Elementgruppe Flex', 'A Content Element for testing purposes.'];
